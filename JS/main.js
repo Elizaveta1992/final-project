1b663e579b8873d04544cee8fd115bbe75a12d0e
lizayurova <li3axop@yandex.ru>
2022-12-04 20:17:18 +0300

    Задание 3 динамический контент
    для карточек "Примеры работ"

--- JS/main.js
+++ JS/main.js
@@ -25,3 +25,43 @@ closeButton.addEventListener('click',() => {
 closeButton.addEventListener( 'click', function (event) {
       alert( 'Вы подписались' );
     } );
+
+
+
+
+      let workEx = [
+        {
+          title: "Для фриланjkljlса",
+          content: "Быстрый поиск работы",
+          img: "images/ex_2.png"
+        },
+        {
+            title: "Для архитекторов",
+            content: "Быстрая обработка чертежей",
+            img: "images/ex_1.png"
+        },
+        {
+            title: "Для фриланса3",
+            content: "Быстрый поиск работы3",
+            img: "images/ex_1.png"        }
+      ];
+
+      let exCardL = document.querySelector('.ex_1');
+      let exImg = exCardL.querySelector('.img_1');
+      let exDescription = exCardL.querySelector('.extext_2');
+      let exTitle = exCardL.querySelector('.extext');
+
+      exTitle.textContent = workEx[0].title;
+      exImg.src = workEx[0].img;
+      exDescription.textContent = workEx[0].content;
+ 
+
+      let exCardR = document.querySelector('.ex_2');
+      let exImgR = exCardR.querySelector('.img_1');
+      let exDescriptionR = exCardR.querySelector('.extext_2');
+      let exTitleR = exCardR.querySelector('.extext');
+
+      exTitleR.textContent = workEx[1].title;
+      exImgR.src = workEx[1].img;
+      exDescriptionR.textContent = workEx[1].content;
+      
